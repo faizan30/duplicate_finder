ba3726840a3a76e88340cea7aacff12568f1a84b
Faiz <faizan.k@botminds.ai>
2023-09-24 15:05:01 +0530

    added vector data manager and helper methods

--- README.md
+++ README.md
@@ -5,10 +5,11 @@ Check if a given text is duplicate of existing text
 # RUN code
 
 ## Create virtual environment
-conda create -y --prefix ./venv python=3.10
-conda activate /home/botml/duplicate_finder/venv
+conda create -y --prefix ./env python=3.7
+conda activate /home/botml/duplicate_finder/env
 
 ## Install dependencies
+conda install -y transformers -c pytorch
 pip install -r requirements.txt
 ## Download models
 
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Union
 
 from fastapi import FastAPI
 from src.duplicate_finder import find_duplicate
+from src.vector_data_manager import write_documents
+from src.vector_data_manager import load_document_store
 import json
 
 app = FastAPI()
@@ -23,4 +25,10 @@ def get_duplicates(text, threshold):
 @app.get("/duplicate")
 def get_duplicates(text="", threshold=0.7):
     return find_duplicate(text, threshold)
+
+@app.get("/write_documents")
+def get_duplicates(text_list=[]):
+    index_path = "document_store/my_faiss_index.faiss"
+    document_store = load_document_store(index_path)
+    return write_documents(text_list, document_store)
     
